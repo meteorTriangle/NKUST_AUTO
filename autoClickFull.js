8a3b2a3a65094173f0ac4a8e4b378c0755b940b8
Triangle Alien(蔡丞銳) <87787306+meteorTriangle@users.noreply.github.com>
2025-01-14 21:28:38 +0800

    Create autoClickFull.js

---
+++ autoClickFull.js
@@ -0,0 +1,29 @@
+var delayTime = 3000;
+
+var a = document.getRootNode();
+var NKUST_LINK = a.location["href"];
+console.log(NKUST_LINK);
+a.body.innerHTML = "";
+var h1 = document.createElement("iframe");
+h1.setAttribute("src", NKUST_LINK.split("/Stu", 1)[0]+"/StuFillIn");
+a.body.appendChild(h1);
+h1.setAttribute("height", 1000);
+h1.setAttribute("width", 1000);
+
+function cdf(){
+var ccgh = h1.contentDocument.getElementById("excollect");
+ccgh.click();
+h1.onload = ccaa;
+}
+
+
+function ccaa(){
+fetch("https://raw.githubusercontent.com/meteorTriangle/NKUST_AUTO/refs/heads/main/autoClick.js").then(response => response.text()).then(data => {
+    var ca = "javascript: "+ data;
+    h1.setAttribute("src", ca);
+});
+h1.onload = cdf;
+};
+
+
+h1.onload = cdf;
